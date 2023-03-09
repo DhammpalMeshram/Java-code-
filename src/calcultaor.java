a2fff7d3b82c5346d947048036b074e235fc8946
Dhammpal <dhammpal123321@gmail.com>
2023-03-09 17:29:12 +0530

    testing java code

---
+++ src/calcultaor.java
@@ -0,0 +1,18 @@
+public class calcultaor {
+    public static void main(String[] args) {
+        int n = 10;
+        int m = 20;
+        int ans = n +m;
+        System.out.println(ans);
+        int [] arr = new int [10];
+        for(int i =0; i<10; i++){
+            arr[i] = 22;
+        }
+        for(int i : arr){
+            System.out.print(i +" ");
+        }
+
+
+    }
+    
+}
